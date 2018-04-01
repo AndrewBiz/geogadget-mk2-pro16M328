3a491ba7e14fac840058dac027a12d456b1e4173
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2018-04-01 21:05:03 +0300

    optimixed init routine for GPS

--- src/gg.cpp
+++ src/gg.cpp
@@ -52,17 +52,18 @@ void setup() {
 
   gps.start_running();
 
-  uint32_t ts = 0;
-  uint32_t ts_show = 0;
+  // uint32_t ts = 0;
+  // uint32_t ts_show = 0;
   do {
     if (gps.available(gpsPort)) {
       fix = gps.read();
-    }
-    ts = millis();
-    if (ts - ts_show > 150) {
       display.show_init_screen(gps, fix);
-      ts_show = ts;
     }
+    // ts = millis();
+    // if (ts - ts_show > 150) {
+    //   display.show_init_screen(gps, fix);
+    //   ts_show = ts;
+    // }
   } while (fix.status < gps_fix::STATUS_STD);
 
   // SD card initializing
--- src/gg_version.hpp
+++ src/gg_version.hpp
@@ -3,7 +3,7 @@
 
 //======================================================================
 #define GG_NAME "Geo-Gadget mk2"
-#define GG_VERSION "2.07.04"
+#define GG_VERSION "2.08.00"
 
 #define GG_ORIGIN "https://github.com/AndrewBiz/geogadget-mk2-pro16M328"
 //======================================================================
